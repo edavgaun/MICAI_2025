55e9535d02fa84a96d66290e9a5479d922d40c42
Edgar Avalos <45240749+edavgaun@users.noreply.github.com>
2025-11-04 15:24:47 -0600

    Update header captions with new references

---
+++ Modules/UI/header.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def show_header(text_title):
+    st.title(text_title)
+    st.caption("📘 Based on: E. Avalos-Gauna et al. (2025, October), *MICAI: 25 Years of AI in Mexico*")
+    st.caption("Avalos-Gauna, E., Palafox, L., & Martinez-Villaseñor, L. (2025, October). MICAI and the Making of AI in Mexico Through 25 Years of Data-Driven Insight. In Mexican International Conference on Artificial Intelligence (pp. 57-72). Cham: Springer Nature Switzerland.")
